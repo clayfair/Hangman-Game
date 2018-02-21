28b0b1d5f4f8f8ec78cbc374a375c6bd049a856e
Clay Fairbanks <clayfairbanks@Clays-MBP.home>
2018-02-21 06:51:49 -0500

    6:51

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,13 @@
-
 //1. list of words for Hangman game. 
 var randomWordsss = ["backpack", "dinosaur", "octopus", "hamburger", "snowman", "popsicle", "inchworm", "skateboard", "caterpillar", "helicopter", "sunglasses", "lollipop", "mountains", "starfish", "feather", "elephant", "videogame", "psychology", "animal"];
 
 //2. random word is chosen from this list 
 var randomWord = randomWordsss[Math.floor(Math.random() * randomWordsss.length)];
 
-//3. new word is chosen each game and appended to the DOM
-window.onload = function beginGame() {
-    var newDiv = document.getElementById("new");
-    var newWord = document.createElement("div");
-    newWord.innerHTML = randomWord;
-    newDiv.appendChild(newWord);
-    console.log(randomWord);
-}
-
-
-//I actually want to index word and place an underscore for each word 
-
-//4. append this new word to the new div 
-
-
-
-
-
-
-
-
-//4. a user has 12 guesses 
+//4. a user has 12 guesses to start. Display on the DOM 
 var userGuessses = 12;
-window.onload = function showGuesses() {
-    var guessDiv = document.getElementById("guesses");
-    var guessesRemaining = document.createElement("div");
-    guessesRemaining.innerHTML = userGuessses;
-    guessDiv.appendChild(guessesRemaining);
+window.onload = function () {
+    document.getElementById('guessesRemaining').innerHTML = userGuessses;
 }
 
 //5. Capture keyboard input
@@ -45,6 +20,7 @@ document.onkeyup = function (event) {
     if (randomWord.indexOf(letter) === -1) {
         //7.1 user guesses decreases by -1
         userGuessses--
+        document.getElementById('guessesRemaining').innerHTML = userGuessses;
         //7.2 append the wrong guessed letter to the "wrong" div. 
         var wrongDiv = document.getElementById("wrong");
         var wrongGuess = document.createElement("div");
@@ -56,4 +32,5 @@ document.onkeyup = function (event) {
     }
 
 }
+
 

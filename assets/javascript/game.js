04b87c96e77ca191f080069c6c523c7b9bc4d003
Clay Fairbanks <clayfairbanks@Clays-MBP.home>
2018-02-21 10:15:39 -0500

    10:15 AM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,32 +5,47 @@ var randomWordsss = ["backpack", "dinosaur", "octopus", "hamburger", "snowman", 
 var randomWord = randomWordsss[Math.floor(Math.random() * randomWordsss.length)];
 
 //4. a user has 12 guesses to start. Display on the DOM 
-var userGuessses = 12;
+var userGuesssesRemaining = 12;
 window.onload = function () {
-    document.getElementById('guessesRemaining').innerHTML = userGuessses;
+    document.getElementById('guessesRemaining').innerHTML = userGuesssesRemaining;
+    console.log(randomWord);
 }
 
-//5. Capture keyboard input
-document.onkeyup = function (event) {
+//5. need to store my guesses to check for repeats later 
+var string = "";
 
-    //6. Captures the key press, converts it to lowercase, and saves it to a variable.
+//6. Capture keyboard input
+document.onkeyup = function (event) {
+    //7. Captures the key press, converts it to lowercase, and saves it to a variable.
     var letter = String.fromCharCode(event.which).toLowerCase();
 
-    //7. when keyboard input is a letter, index the randomWord and check if that letter exists. If it doesn't...
+    //8. when keyboard input = letter, index my randomWord and check if that letter exists. If it doesn't...
     if (randomWord.indexOf(letter) === -1) {
-        //7.1 user guesses decreases by -1
-        userGuessses--
-        document.getElementById('guessesRemaining').innerHTML = userGuessses;
-        //7.2 append the wrong guessed letter to the "wrong" div. 
-        var wrongDiv = document.getElementById("wrong");
-        var wrongGuess = document.createElement("div");
-        wrongGuess.innerHTML = letter;
-        wrongDiv.appendChild(wrongGuess);
-        console.log("letters already guessed:" + letter);
+        //8.1 push all wrong guesses into my string. 
+        string = letter + string;
+        //8.2 Check this string against my keyboard letter for repeats
+        if (string.indexOf(letter, 1) === -1) {
+            //8.3 append guess to the DOM
+            var alreadyDiv = document.getElementById('already');
+            var newDiv = document.createElement("div");
+            newDiv.innerHTML = letter;
+            alreadyDiv.appendChild(newDiv);
+            //8.3 decrease user guess by 1
+            userGuesssesRemaining--
+            //8.4 update the DOM with userGuesses remaining 
+            document.getElementById('guessesRemaining').innerHTML = userGuesssesRemaining;
+        } else {
+            return "repeat"
+        }
+
+
     } else {
-        console.log("correct!");
+        //8 if the letter does exist then we have correctly chosen a letter 
+        console.log("correct letter!: " + letter);
     }
 
 }
+
+
 
 
